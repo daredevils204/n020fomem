a23dfe230ea327df164f929f0dae3cfd36049f77
daredevils204 <dayn01914@gmail.com>
2025-02-05 22:08:43 +0700

    Update icons.tsx

--- components/ui/icons.tsx
+++ components/ui/icons.tsx
@@ -2,9 +2,22 @@
 
 import { cn } from '@/lib/utils'
 
-function IconLogo({ className, ...props }: React.ComponentProps<'img'>) {
+function IconLogo({ className, ...props }: React.ComponentProps<'svg'>) {
   return (
-    <img src="nexa-logo.png" />
+    <svg version="1.0" xmlns="http://www.w3.org/2000/svg"
+ width="40" height="40" viewBox="0 0 300.000000 300.000000"
+ preserveAspectRatio="xMidYMid meet">
+<g transform="translate(0.000000,300.000000) scale(0.100000,-0.100000)"
+fill="#000000" stroke="none">
+<path d="M1375 2689 c-355 -39 -673 -232 -868 -525 -96 -145 -147 -267 -183
+-436 -24 -115 -24 -342 0 -458 101 -476 458 -837 931 -941 140 -31 349 -31
+490 1 475 104 822 457 932 945 10 47 16 120 16 225 0 165 -10 229 -58 375
+-147 447 -551 769 -1025 815 -107 11 -136 11 -235 -1z m24 -1192 l210 -367
+-425 0 c-233 0 -424 2 -424 4 0 4 383 676 407 714 8 13 16 22 18 20 2 -2 98
+-169 214 -371z m841 367 c0 -12 -415 -734 -421 -734 -4 0 -96 156 -204 348
+-109 191 -205 357 -213 370 l-14 22 426 0 c234 0 426 -3 426 -6z"/>
+</g>
+</svg>
   )
 }
 
