cfb913afc33ad1b26f02928eb5bd1664f79f3f1f
daredevils204 <dayn01914@gmail.com>
2025-02-05 21:39:32 +0700

    Update footer.tsx

--- components/footer.tsx
+++ components/footer.tsx
@@ -7,33 +7,7 @@ const Footer: React.FC = () => {
   return (
     <footer className="w-fit p-1 md:p-2 fixed bottom-0 right-0 hidden lg:block">
       <div className="flex justify-end">
-        <Button
-          variant={'ghost'}
-          size={'icon'}
-          className="text-muted-foreground/50"
-        >
-          <Link href="https://discord.gg/zRxaseCuGq" target="_blank">
-            <SiDiscord size={18} />
-          </Link>
-        </Button>
-        <Button
-          variant={'ghost'}
-          size={'icon'}
-          className="text-muted-foreground/50"
-        >
-          <Link href="https://x.com/morphic_ai" target="_blank">
-            <SiX size={18} />
-          </Link>
-        </Button>
-        <Button
-          variant={'ghost'}
-          size={'icon'}
-          className="text-muted-foreground/50"
-        >
-          <Link href="https://git.new/morphic" target="_blank">
-            <SiGithub size={18} />
-          </Link>
-        </Button>
+        <p className="text-gray-500 dark:text-gray-400">© Copyright 2025 by Nexa LAC</p>
       </div>
     </footer>
   )
