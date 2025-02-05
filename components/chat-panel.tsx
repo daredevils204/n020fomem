488bb4ead2d90e9caac5e4aacffe0312b1651169
daredevils204 <dayn01914@gmail.com>
2025-02-05 22:23:37 +0700

    Update chat-panel.tsx

--- components/chat-panel.tsx
+++ components/chat-panel.tsx
@@ -80,7 +80,7 @@ export function ChatPanel({
     >
       {messages.length === 0 && (
         <div className="mb-8">
-          <h5 class="text-xl text-black tracking-tight font-bold dark:text-white">nexa</h5>
+          <h5 className="text-xl text-black tracking-tight font-bold dark:text-white">nexa</h5>
         </div>
       )}
       <form
