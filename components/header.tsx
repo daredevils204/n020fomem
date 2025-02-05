f542b451f2524f854c811db8c73fca8e8e490c2a
daredevils204 <dayn01914@gmail.com>
2025-02-05 22:19:04 +0700

    Update header.tsx

--- components/header.tsx
+++ components/header.tsx
@@ -9,7 +9,7 @@ export const Header: React.FC = async () => {
     <header className="fixed w-full p-2 flex justify-between items-center z-10 backdrop-blur md:backdrop-blur-none bg-background/80 md:bg-transparent">
       <div>
         <a href="/">
-          <p className="text-black tracking-tight dark:text-white">nexa</p>
+          <p className="font-bold ml-2 text-black tracking-tight dark:text-white">nexa</p>
           <span className="sr-only">nexa</span>
         </a>
       </div>
