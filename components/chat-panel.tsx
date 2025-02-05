41a99183aa6d223596fd78733a7cc30b8c0f19fc
daredevils204 <dayn01914@gmail.com>
2025-02-05 20:56:39 +0700

    Update chat-panel.tsx

--- components/chat-panel.tsx
+++ components/chat-panel.tsx
@@ -80,7 +80,7 @@ export function ChatPanel({
     >
       {messages.length === 0 && (
         <div className="mb-8">
-          <IconLogo className="size-12 text-muted-foreground" />
+          <img className="size-12 text-muted-foreground" src="IMG_7118.png" alt="nexa logo" />
         </div>
       )}
       <form
