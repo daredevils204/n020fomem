27367d735c8af18b67418543cf009fb3ef5156f6
daredevils204 <dayn01914@gmail.com>
2025-02-05 21:13:52 +0700

    Update icons.tsx

--- components/ui/icons.tsx
+++ components/ui/icons.tsx
@@ -5,16 +5,24 @@ import { cn } from '@/lib/utils'
 function IconLogo({ className, ...props }: React.ComponentProps<'svg'>) {
   return (
     <svg
-      fill="currentColor"
-      viewBox="0 0 256 256"
-      role="img"
-      xmlns="http://www.w3.org/2000/svg"
-      className={cn('h-4 w-4', className)}
-      {...props}
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 300 300"
+    preserveAspectRatio="xMidYMid meet"
+    {...props}
     >
-      <circle cx="128" cy="128" r="128" fill="black"></circle>
-      <circle cx="102" cy="128" r="18" fill="white"></circle>
-      <circle cx="154" cy="128" r="18" fill="white"></circle>
+      <g
+      transform="translate(0,300) scale(0.1,-0.1)"
+      fill="currentColor"
+      stroke="none"
+      >
+        <path d="M1375 2689 c-355 -39 -673 -232 -868 -525 -96 -145 -147 -267 -183
+      -436 -24 -115 -24 -342 0 -458 101 -476 458 -837 931 -941 140 -31 349 -31
+      490 1 475 104 822 457 932 945 10 47 16 120 16 225 0 165 -10 229 -58 375
+      -147 447 -551 769 -1025 815 -107 11 -136 11 -235 -1z m24 -1192 l210 -367
+      -425 0 c-233 0 -424 2 -424 4 0 4 383 676 407 714 8 13 16 22 18 20 2 -2 98
+      -169 214 -371z m841 367 c0 -12 -415 -734 -421 -734 -4 0 -96 156 -204 348
+      -109 191 -205 357 -213 370 l-14 22 426 0 c234 0 426 -3 426 -6z" />
+      </g>
     </svg>
   )
 }
