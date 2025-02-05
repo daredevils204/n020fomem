66cdabb889fd11469fed3592d75e065076430bc9
daredevils204 <dayn01914@gmail.com>
2025-02-05 22:02:39 +0700

    Update icons.tsx

--- components/ui/icons.tsx
+++ components/ui/icons.tsx
@@ -2,7 +2,7 @@
 
 import { cn } from '@/lib/utils'
 
-function IconLogo({ className, ...props }: React.ComponentProps<'svg'>) {
+function IconLogo({ className, ...props }: React.ComponentProps<'img'>) {
   return (
     <img src="nexa-logo.png" />
   )
