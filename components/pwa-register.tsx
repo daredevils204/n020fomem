00fc26d9d87aad75ed17e824a16fc5b7797e7ee5
daredevils204 <dayn01914@gmail.com>
2025-02-06 19:27:02 +0700

    Update pwa-register.tsx

--- components/pwa-register.tsx
+++ components/pwa-register.tsx
@@ -8,10 +8,10 @@ export function PWARegister() {
       window.addEventListener("load", () => {
         navigator.serviceWorker.register("/sw.js").then(
           (registration) => {
-            console.log("ServiceWorker registration successful with scope: ", registration.scope)
+            console.log("Đăng ký ServiceWorker thành công với phạm vi: ", registration.scope)
           },
           (err) => {
-            console.log("ServiceWorker registration failed: ", err)
+            console.log("Đăng ký ServiceWorker không thành công: ", err)
           },
         )
       })
