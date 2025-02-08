b67b57bd7817b521621e7be08fcd23b36dd494db
daredevils204 <dayn01914@gmail.com>
2025-02-08 12:30:01 +0700

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,25 +1,7 @@
-import { useAuth } from "@/contexts/auth-context"
 import { Chat } from '@/components/chat'
 import { generateId } from 'ai'
 
 export default function Page() {
-  const { user, loading } = useAuth()
-
-  if (loading) {
-    return <p>Loading...</p>
-  }
-
-  if (user) {
-    return (
-      const id = generateId()
+  const id = generateId()
   return <Chat id={id} />
-    )
-  }
-
-  return (
-    <div>
-      <p>Please login</p>
-      <a href="/login">Login</a>
-    </div>
-  )
 }
