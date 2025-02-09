c5dd660ea5324510ab71b40a72782e8c6f9d6f97
daredevils204 <dayn01914@gmail.com>
2025-02-10 05:07:50 +0700

    Update header.tsx

--- components/header.tsx
+++ components/header.tsx
@@ -9,7 +9,7 @@ export const Header: React.FC = async () => {
     <header className="fixed w-full p-2 flex justify-between items-center z-10 backdrop-blur md:backdrop-blur-none bg-background/80 md:bg-transparent">
       <div>
         <a href="/">
-          <IconLogo className={cn('w-5 h-5')} />
+          <IconLogo className={cn('ml-2 w-5 h-5')} />
           <span className="sr-only">nexa</span>
         </a>
       </div>
