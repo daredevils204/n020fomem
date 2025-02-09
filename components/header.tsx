9a3765d1b168f78cee6e63bbe6c3c40769c27dbc
daredevils204 <dayn01914@gmail.com>
2025-02-09 19:24:55 +0700

    Update header.tsx

--- components/header.tsx
+++ components/header.tsx
@@ -5,6 +5,10 @@ import { ModeToggle } from './mode-toggle'
 import { IconLogo } from './ui/icons'
 import { Chat } from '@/lib/types'
 
+type HistoryItemProps = {
+  chat: Chat
+}
+
 export const Header: React.FC = async () => {
   return (
     <header className="fixed w-full p-2 flex justify-between items-center z-10 backdrop-blur md:backdrop-blur-none bg-background/80 md:bg-transparent">
