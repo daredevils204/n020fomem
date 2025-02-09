30db7eedf0e992563dd8539af9fbbe35078f56ac
daredevils204 <dayn01914@gmail.com>
2025-02-09 19:33:01 +0700

    Update header.tsx

--- components/header.tsx
+++ components/header.tsx
@@ -3,16 +3,8 @@ import React from 'react'
 import HistoryContainer from './history-container'
 import { ModeToggle } from './mode-toggle'
 import { IconLogo } from './ui/icons'
-import { Chat } from '@/lib/types'
 
-type HistoryItemProps = {
-  chat: Chat
-}
-
-export const Header: React.FC<HistoryItemProps> = async ({ chat }) => {
-  const pathname = usePathname()
-  const isActive = pathname === chat.path
-
+export const Header: React.FC = async () => {
   return (
     <header className="fixed w-full p-2 flex justify-between items-center z-10 backdrop-blur md:backdrop-blur-none bg-background/80 md:bg-transparent">
       <div>
@@ -20,11 +12,6 @@ export const Header: React.FC<HistoryItemProps> = async ({ chat }) => {
           <IconLogo className={cn('w-5 h-5')} />
           <span className="sr-only">nexa</span>
         </a>
-      </div>
-      <div>
-          <div className="text-xs font-medium truncate select-none">
-            {chat.title}
-          </div>
       </div>
       <div className="flex gap-0.5">
         <ModeToggle />
